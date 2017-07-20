f86eb11eb1db7e13de3c66e7a3200a12f63dfced
EvgenyPopov <36furious@gmail.com>
2017-07-20 18:59:06 +0300

    add Hello World #task 141

---
+++ ex_1/src/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ * Calculate.
+ *
+ * @author Evgeny Popov (36furious@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
