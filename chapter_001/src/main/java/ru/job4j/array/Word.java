3bd669c64b29e13d07d429d2193680ef63e9e0f7
EvgenyPopov <36furious@gmail.com>
2017-09-04 10:32:27 +0300

    Complite  task [#228]

--- chapter_001/src/main/java/ru/job4j/array/Word.java
+++ chapter_001/src/main/java/ru/job4j/array/Word.java
@@ -1,13 +1,31 @@
 package ru.job4j.array;
 
+/**
+ * @author Evgeny Popov
+ * @since 1.0
+ */
+
 public class Word {
+
+    /**
+     * Conversion of String in char array.
+     * @param temp - Conversion word.
+     * @return Char array.
+     */
+
 
     public char[] wordInArray (String temp){
        return temp.toCharArray();
     }
 
+    /**
+     * Check that one word contain in two word.
+     * @param origin - Conversion word.
+     * @param sub - The word that we are looking for
+     * @return boolean value
+     */
     boolean contains(char[] origin, char[] sub){
-        int key = 0, sublenth = sub.length - 1;
+        int key = 0, sublenth = sub.length - 1; //sublenth - Length check the word.
         for (int i = 0; i < origin.length - 1; i++) {
             if (origin[i] == sub[key]) {
                 key++;
--- chapter_001/src/test/java/ru/job4j/array/WordTest.java
+++ chapter_001/src/test/java/ru/job4j/array/WordTest.java
@@ -5,6 +5,14 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test.
+ ** @author Evgeny Popov (mailto:36furious@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ * This test check that one word contain in two word.
+ */
+
 public class WordTest {
     @Test
     public void whenOneWordInOtherWordThenTrue() {
@@ -16,5 +24,14 @@ public class WordTest {
         assertThat(result, is(expected));
     }
 
+    @Test
+    public void whenOneWordInOtherWordThenFalse() {
+        Word word = new Word();
+        String origin = "Досвидания";
+        String sub = "иве";
+        Boolean result = word.contains(word.wordInArray(origin), word.wordInArray(sub));
+        Boolean expected = false;
+        assertThat(result, is(expected));
+    }
 
 }
